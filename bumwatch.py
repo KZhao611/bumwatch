85e8102949636f87a6b5750f72685b53219c5a02
Kevin <kevinzhao611@gmail.com>
2024-07-14 02:18:27 -0400

    turned commands into slash commands

--- bumwatch.py
+++ bumwatch.py
@@ -3,6 +3,8 @@ import os
 import sqlite3
 import discord
 from discord.ext import commands
+from discord import app_commands
+from league_methods import *
 
 load_dotenv()
 
@@ -11,38 +13,83 @@ cur = con.cursor()
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = commands.Bot(command_prefix='/', intents=intents)
 
 player = False
+guild=discord.Object(id=971400515600658464)
 
 @client.event
 async def on_ready():
+    await client.tree.sync(guild=guild)
     print(f'We have logged in as {client.user}')
 
-@client.command()
-async def ping(ctx, members: commands.Greedy[discord.Member]):
-    for member in members:
-        await ctx.send(member.global_name)
-
-@client.command()
-async def log(ctx):
+@client.tree.command(
+    name="ping",
+    description="testing",
+    guild=guild
+)
+async def ping(interaction:discord.Interaction, member: discord.Member):
+    await interaction.response.send_message(f"@{member.global_name}", ephemeral=False)
+   
+@client.tree.command(
+    name="log",
+    description="Logs current player being tracked",
+    guild=guild
+)
+async def ping(interaction:discord.Interaction):
     if(not player):
-        await ctx.send("No player currently being tracked... Use /track to add them.")
+        await interaction.response.send_message("No player currently being tracked... Use /track to add them.")
     else:
-        await ctx.send(f"Currently tracking {player}")
+        await interaction.response.send_message(f"Currently tracking {player}")
 
-@client.command()
-async def track(ctx, username):
-    player = username
-    await ctx.send(f"Currently tracking {player}")
+@client.tree.command(
+    name="track",
+    description="Adds player to be tracked",
+    guild=guild
+)
+async def track(interaction:discord.Interaction, person: discord.Member):
+    player = cur.execute("SELECT riot FROM players WHERE discord = ?", (person.id,)).fetchone()
+    if (player == None):
+        await interaction.response.send_message(f"Player {person.display_name} is not registered. Use /register to register them.")
+    else:
+        await interaction.response.send_message(f"Currently tracking {person.display_name}")
+        player = player[0]
+
     
-@client.command()
-async def register(ctx, discordUser, leagueId):
-    cur.execute("INSERT INTO players VALUES ? ?", discordUser, leagueId)
-    con.commit()
+@client.tree.command(
+        name="register",
+        description="Register your riot account",
+        guild=guild
+)
+@app_commands.describe(league_user="Username#Tagline")
+@app_commands.rename(league_user="riot_id")
+async def register(interaction: discord.Interaction, league_user: str):
+    try:
+        riotID = search_riot_id(league_user)
+        cur.execute("INSERT INTO players VALUES (?, ?) ON CONFLICT (discord) DO UPDATE SET riot = excluded.riot", (interaction.user.id, riotID))
+        con.commit()
+        await interaction.response.send_message("Registered!", ephemeral=True)
+    except Exception as e:
+        await interaction.response.send_message("Unable to find player, make sure the entered Username#Tagline is correct!", ephemeral=True)
+        print(e)
 
-@client.command()
+
+@client.tree.command(
+    name="unregister",
+    description="Unregister your riot account",
+    guild=guild
+)
+async def unregister(interaction:discord.Interaction):
+    cur.execute("DELETE FROM players WHERE discord = ?", (interaction.user.id,))
+    con.commit()
+    await interaction.response.send_message("Unregistered!", ephemeral=True)
+
+# #work on moving methods into db _file
+
+@client.tree.command(
+    name="logdb",
+    guild=guild
+)
 async def logDB(ctx):
     res = cur.execute("SELECT * from players")
     print(res.fetchall())
--- league_methods.py
+++ league_methods.py
@@ -1,5 +1,17 @@
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
+
+api_key = os.getenv("RIOT_API_KEY")
+
+def search_riot_id(str):
+    gameName, tagLine = str.split("#")
+    region = "americas"
+    res = requests.get(f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}?api_key={api_key}")
+    if res.status_code != 200:
+        raise Exception("Unable to get PUUID")
+    data = res.json()
+    return(data["puuid"])
 
